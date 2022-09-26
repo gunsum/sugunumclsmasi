0a7306c0557be8ec807e4ca8545bfe695cc8c522
Fatma Oymak <sugunum@gmail.com>
2022-09-26 21:33:09 +0100

    h2 ekledim

--- su/src/App.js
+++ su/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>su yun sayfasi</h1>
+      <h2>GUN un basligi</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
